c15f69a15b598fe357f39def559a286f55cef537
TotallyNotHuman <tnhthereal@gmail.com>
2017-08-07 09:36:20 -0700

    KSPNameGen v0.3.0

--- KSPNameGen/KSPNameGen.cs
+++ KSPNameGen/KSPNameGen.cs
@@ -54,8 +54,8 @@ namespace KSPNameGen
 		// static version defs
 
 		const ushort MAJOR = 0;
-		const ushort MINOR = 2;
-		const ushort PATCH = 1;
+		const ushort MINOR = 3;
+		const ushort PATCH = 0;
 		const string SUFFX = "";
 
 		// variable definitions
