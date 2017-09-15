00901a211d865b740e16b999f9a293cb3aed1652
TotallyNotHuman <tnhthereal@gmail.com>
2017-09-15 14:40:21 -0400

    Fix version generation script

--- scripts/genver.sh
+++ scripts/genver.sh
@@ -4,9 +4,11 @@ gitrevs="$(git rev-list v$stable..HEAD 2> /dev/null | wc -l)"
 githash="$(git rev-parse --short HEAD 2> /dev/null)"
 
 if [ "$gitrevs" -eq "0" ]; then
-    gitrevs="."
+    unset gitrevs
 fi
 
-gitrevs="."$gitrevs
+if [ -n "$gitrevs" ]; then
+    gitrevs=".$gitrevs"
+fi
 
-printf "namespace KSPNameGen\n{\n\tpublic static class ProductVersion\n\t{\n\t\tpublic const string productVersion = \"%s%s-g%s\";\n\t}\n}" "$stable" "$gitrevs" "$githash" > KSPNameGen/ProductVersion.cs
+printf "namespace KSPNameGen\n{\n\tpublic static class ProductVersion\n\t{\n\t\tpublic const string productVersion = \"%s%s-g%s\";\n\t}\n}\n" "$stable" "$gitrevs" "$githash" > KSPNameGen/ProductVersion.cs
